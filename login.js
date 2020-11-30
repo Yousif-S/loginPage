939a90172662385c908abd6c098b75900f0e0d38
Yousif-S <71367927+Yousif-S@users.noreply.github.com>
2020-11-30 17:43:49 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,47 @@
+
+              
+               function login()
+            {
+                   var name = document.login.Username.value;
+                    var pass = document.login.Password.value; 
+                    
+                   var valid=true;
+              
+              if(name === "")
+              {
+                  
+              valid=false; 
+                Uname.style.visibility = "visible"; 
+                
+            }
+               else{
+                   
+               Uname.style.visibility = "hidden"; 
+               
+                    } 
+
+              if(pass === "")
+              {
+                  
+              valid=false; 
+                Epass.style.visibility = "visible"; 
+                
+            }
+               else{
+                   
+               Epass.style.visibility = "hidden"; 
+               
+                    } 
+           
+              if(valid)
+              {
+                 return true;
+              }
+              else
+              {
+                  return false;
+              }
+              
+              
+            }
+      

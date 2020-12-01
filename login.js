5e190ae5d3ea09317a98a02a6840b323ff4baf5d
Yousif-S <71367927+Yousif-S@users.noreply.github.com>
2020-12-01 21:54:55 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,47 +1,35 @@
-
-              
-               function login_valid()
-            {
-                   var name = document.login.Username.value;
-                    var pass = document.login.Password.value; 
-                    
-                   var valid=true;
-              
-              if(name === "")
-              {
-                  
-              valid=false; 
-                Uname.style.visibility = "visible"; 
-                
-            }
-               else{
-                   
-               Uname.style.visibility = "hidden"; 
-               
-                    } 
-
-              if(pass === "")
-              {
-                  
-              valid=false; 
-                Epass.style.visibility = "visible"; 
-                
-            }
-               else{
-                   
-               Epass.style.visibility = "hidden"; 
-               
-                    } 
-           
-              if(valid)
-              {
-                 return true;
-              }
-              else
-              {
-                  return false;
-              }
-              
-              
-            }
-      
+function login_valid()
+{
+  var User = document.login.Username.value;  
+  var Pass = document.login.Password.value; 
+  var valid = true; 
+  
+  if(User === "")
+  {
+      valid=false;
+      euser.style.visibility = "visible"; 
+  }
+  else
+  {
+     euser.style.visibility = "hidden"; 
+  }
+  
+    if(Pass === "")
+  {
+      valid=false;
+      epass.style.visibility = "visible"; 
+  }
+  else
+  {
+     epass.style.visibility = "hidden"; 
+  }
+  
+  if(valid)
+  {
+      return true;
+  }
+  else
+  {
+      return false;
+  }
+}
